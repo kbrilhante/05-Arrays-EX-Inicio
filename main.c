a3a1d2adc05b3963ae36d35ed515e93a7b821532
Karina <kbrilhante@gmail.com>
2024-09-03 23:01:05 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#define ex00
+
+/*** EXERCICIOS ARRAYS ***/
+
+/* 
+	* Os 3 primeiros exercícios podem ser feitos em um só se quiser
+	* Em todos os desafios o array pode ser hardcoded (inicializado direto no código)
+	por exemplo:
+	int array[] = {33, 18, 52, 59, 51, 74, 40, 79, 11, 65};
+*/
+
+#ifdef ex01
+/*
+	DESAFIO: Escreva um programa que recebe o valor máximo de um array
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex02
+/*
+	DESAFIO: Escreva um programa que recebe o valor mínimo de um array
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex03
+/*
+	DESAFIO: Escreva um programa que recebe um array e retorna a soma dos seus elementos
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex04
+/*
+	DESAFIO: Escreva um programa que inverte a sequência de um array.
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex05
+/*
+	DESAFIO AINDA MAIS DESAFIADOR: Faça isso sem criar um segundo array
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
